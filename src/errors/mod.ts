24536a9fdc92a21bcbbe97beb97fd0f3c502b97c
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-10-21 17:23:00 +0200

    add a ChildProcessNotRunningError

---
+++ src/errors/mod.ts
@@ -0,0 +1,5 @@
+/**
+ * This error is thrown when a child process is not running, but an operation is attempted that requires the child process to be running.
+ */
+export class ChildProcessNotRunningError extends Error {
+}
