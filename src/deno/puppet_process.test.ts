b7a7fb18d6390949a771500c773ee1a136b5558c
Benjamin Jesuiter <bjesuiter@gmail.com>
2024-10-21 13:24:38 +0200

    allow waiting for puppet process exit

--- src/deno/puppet_process.test.ts
+++ src/deno/puppet_process.test.ts
@@ -1,11 +1,16 @@
 import { assertExists } from "@std/assert";
 import { PuppetProcess } from "./puppet_process.ts";
 
-Deno.test("Deno PuppetProcess init", () => {
+Deno.test("Deno PuppetProcess init", async () => {
     const process = new PuppetProcess({
-        executable: "echo",
-        args: ["Hello, world!"],
+        command: `echo "Hello, world!"`,
     });
 
+    process.std_in.getWriter().write("Hello, world!");
+
     assertExists(process);
+
+    process.start();
+
+    await process.waitForExit();
 });
--- src/deno/puppet_process.ts
+++ src/deno/puppet_process.ts
@@ -1,16 +1,104 @@
 import type { GenericLogger } from "../shared/GenericLogger.type.ts";
+import { simpleCallbackTarget } from "@codemonument/rx-webstreams";
 
 export type PuppetProcessOptions = {
-    executable: string;
-    args: string[];
+    /**
+     * The command which should be run as {@link PuppetProcess}.
+     * - Format: Space-Delimited String: `echo "Hello, world!"`
+     * - params for the command can be interpolated by using `${param}` and passing the resulting string to {@link PuppetProcess}.
+     *   Example: `echo "Hello, ${name}!"`
+     */
+    command: string;
+
+    /**
+     * An optional instance of a logger.
+     * If not provided, the default logger will be `console`.
+     * Can be used to pipe log messages to a custom logger.
+     */
     logger?: GenericLogger;
 };
 
+/**
+ * This class is used to spawn a new process and interact with it.
+ * It is NOT inteded to run child processes one-time, but rather to keep them running and interact with them.
+ * (At least for now, maybe I'll add this functionality later.)
+ */
 export class PuppetProcess {
-    logger: GenericLogger;
+    private options: PuppetProcessOptions;
+    private logger: GenericLogger;
+    private textEncoder = new TextEncoder();
+
+    // Runtime Props
+    private cmd: Deno.Command;
+    private child?: Deno.ChildProcess;
+    private std_in_transform = new TransformStream<
+        string | Uint8Array,
+        Uint8Array
+    >({
+        start: (_controller) => {
+        },
+        transform: (chunk, controller) => {
+            if (typeof chunk === "string") {
+                controller.enqueue(this.textEncoder.encode(chunk));
+            } else {
+                controller.enqueue(chunk);
+            }
+        },
+    });
+
+    // Public Props
+    /**
+     * The output stream of the child process.
+     */
+    public readonly std_out = new ReadableStream();
+    public readonly std_err = new ReadableStream();
+    public readonly std_all = new ReadableStream();
+    public readonly std_in = this.std_in_transform.writable;
 
     constructor(options: PuppetProcessOptions) {
+        this.options = options;
         this.logger = options.logger ?? console as GenericLogger;
         this.logger.log("PuppetProcess created with options:", options);
+
+        const [executable, ...args] = options.command.split(" ");
+        const cmd = new Deno.Command(executable, {
+            args,
+            stdout: "piped",
+            stderr: "piped",
+        });
+        this.cmd = cmd;
+    }
+
+    start() {
+        this.child = this.cmd.spawn();
+
+        this.std_in_transform.readable.pipeTo(simpleCallbackTarget((chunk) => {
+            this.logger.log(`stdin: ${chunk}`);
+        }));
+    }
+
+    async waitForExit(): Promise<void> {
+        await this.child?.output();
+    }
+
+    /**
+     * @returns `true` if the process was successfully killed, `false` if the process was not running or an error happened while killing the process.
+     */
+    kill(): boolean {
+        if (!this.child) {
+            this.logger.warn(
+                "Attempted to kill a PuppetProcess that is not running.",
+            );
+            return false;
+        }
+
+        try {
+            this.child?.kill();
+        } catch (error) {
+            this.logger.error("Error while killing process:", error);
+            return false;
+        }
+
+        return true;
     }
 }
